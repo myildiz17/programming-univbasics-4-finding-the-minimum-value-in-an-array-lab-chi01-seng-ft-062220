8147d9fe2d3eb7709eb578ccc3a825a08f7c8103
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-05-02 05:08:15 +0000

    Automatically backed up by Learn

--- mine.rb
+++ mine.rb
@@ -1,5 +1,9 @@
 
+<<<<<<< HEAD
 array=[3,8,0,5,1,9]
+=======
+array=[8,2,5,1,9]
+>>>>>>> 259b4475c8f4b43b6b17ee769611c6b57677d373
 count=0
 min_value_index=0
 
@@ -7,6 +11,7 @@ min_value_index=0
 
 while count < array.size do
   if array[min_value_index] < array[count]
+<<<<<<< HEAD
 
   else
   min_value_index=count
@@ -15,3 +20,13 @@ count += 1
 end
 
 puts array[min_value_index]
+=======
+puts "no"
+  else
+  puts "yes"
+  end
+count =+1
+end
+
+#puts array[min_value_index]
+>>>>>>> 259b4475c8f4b43b6b17ee769611c6b57677d373
